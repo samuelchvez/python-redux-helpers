3d0765d457ec061306616cd7f6cd687de23e2371
Samuel Chávez <schavezfuentes@gmail.com>
2020-12-11 10:52:09 -0600

    Remove typings

--- templates.py
+++ templates.py
@@ -1,37 +1,17 @@
-domain_type_template = '{domain}_TYPE'
-
-type_template = '''export type {type_name} = {{
-  type: '{type_value}',
-  payload: {{}},
-}};
-export const {type_value} = '{type_value}';
+type_template = '''export const {type_value} = '{type_value}';
 
 '''
 
-types_file_template = '''// @flow
-import type {{ ID_TYPE, ERROR_TYPE }} from './common';
+types_file_template = ''''''
 
-export type {domain_type} = {{}};
-
-{types}export type {domain}_ACTION_TYPE =
-  | {or_type};
-'''
-
-action_template = '''export const {action_name} = (): {type_name} => ({{
+action_template = '''export const {action_name} = () => ({{
   type: types.{type_value},
   payload: {{}},
 }});
 
 '''
 
-actions_file_template = '''// @flow
-import type {{ ID_TYPE, ERROR_TYPE }} from '../types/common';
-
-import type {{
-  {domain_type},
-{flow_types},
-}} from '../types/{domain}';
-import * as types from '../types/{domain}';
+actions_file_template = '''import * as types from '../types/{domain}';
 
 
 {actions}
@@ -43,25 +23,10 @@ reducer_case_template = '''
   }}
 '''
 
-reducers_file_template = '''//@flow
-import {{ combineReducers }} from 'redux';
+reducers_file_template = '''import {{ combineReducers }} from 'redux';
 
-import type {{ ID_TYPE, ERROR_TYPE }} from '../types/common';
-import type {{ {domain_type} }} from '../types/{domain}';
 import * as common from './common';
 import * as types from '../types/{domain}';
-
-export type {domain_state_type} = {{
-  byId: {{ [ID_TYPE]: {domain_type} }},
-  order: Array<ID_TYPE>,
-  fetching: Array<ID_TYPE>,
-  isFetching: boolean,
-  error: ERROR_TYPE,
-  errors: {{ [ID_TYPE]: ERROR_TYPE }},
-  isToggled: boolean,
-  selected: number,
-  counter: number,
-}};
 
 const byId = common.byId({{
   added: [],
@@ -142,28 +107,22 @@ export default {domain};
 
 
 // Selectors
-export const get{singular_upper_camel_domain} = (state: {domain_state_type}, id: ID_TYPE): ?{domain_type} => state.byId[id];
-export const get{upper_camel_domain} = (state: {domain_state_type}): Array<?{domain_type}> => state.order.map(i => get{singular_upper_camel_domain}(state, i));
-export const is{singular_upper_camel_domain}Fetching = (state: {domain_state_type}, id: ID_TYPE): boolean => state.fetching.includes(id);
-export const isFetching{upper_camel_domain} = (state: {domain_state_type}): boolean => state.isFetching;
-export const get{upper_camel_domain}Error = (state: {domain_state_type}): ERROR_TYPE => state.error;
-export const get{singular_upper_camel_domain}Error = (state: {domain_state_type}, id: ID_TYPE): ERROR_TYPE => state.errors[id];
-export const are{upper_camel_domain}Toggled = (state: {domain_state_type}): boolean => state.toggle;
-export const getSelected{singular_upper_camel_domain} = (state: {domain_state_type}): ?{domain_type} => get{singular_upper_camel_domain}(state, state.selected);
-export const get{singular_upper_camel_domain}Counter = (state: {domain_state_type}): number => state.counter;
+export const get{singular_upper_camel_domain} = (state, id) => state.byId[id];
+export const get{upper_camel_domain} = state => state.order.map(i => get{singular_upper_camel_domain}(state, i));
+export const is{singular_upper_camel_domain}Fetching = (state, id) => state.fetching.includes(id);
+export const isFetching{upper_camel_domain} = state => state.isFetching;
+export const get{upper_camel_domain}Error = state => state.error;
+export const get{singular_upper_camel_domain}Error = (state, id => state.errors[id];
+export const are{upper_camel_domain}Toggled = state => state.toggle;
+export const getSelected{singular_upper_camel_domain} = state => get{singular_upper_camel_domain}(state, state.selected);
+export const get{singular_upper_camel_domain}Counter = state: number => state.counter;
 
 //////////////////////////////////////////////////////////////
 // TODO: move to index.js
 //////////////////////////////////////////////////////////////
 
 // Imports
-import type {{ {domain_state_type} }} from './{domain}';
 import {domain}, * as from{upper_camel_domain} from './{domain}';
-
-// AppState
-export type AppState = {{
-  {domain}: {domain_state_type}
-}};
 
 // Reducer
 const reducer = combineReducers({{
