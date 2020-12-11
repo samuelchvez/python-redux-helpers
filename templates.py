499c6de8e4d3dbcef11860f522aa7b43a374f64f
Samuel Chávez <schavezfuentes@gmail.com>
2020-12-11 11:03:57 -0600

    Removed types from templates

--- templates.py
+++ templates.py
@@ -1,37 +1,19 @@
 add_type_template = '{to_type}_TYPE'
 
-type_template = '''export type {type_name} = {{
-  type: '{type_value}',
-  payload: {{}},
-}};
-export const {type_value} = '{type_value}';
+type_template = '''export const {type_value} = '{type_value}';
 
 '''
 
-types_file_template = '''// @flow
-import type {{ ID_TYPE, ERROR_TYPE }} from './common';
+types_file_template = ''''''
 
-export type {managed_type} = {{}};
-
-{types}export type {main_action_type_name}_ACTION_TYPE =
-  | {or_type};
-'''
-
-action_template = '''export const {action_name} = (): {type_name} => ({{
+action_template = '''export const {action_name} = () => ({{
   type: types.{type_value},
   payload: {{}},
 }});
 
 '''
 
-actions_file_template = '''// @flow
-import type {{ ID_TYPE, ERROR_TYPE }} from '../types/common';
-
-import type {{
-  {managed_type},
-{flow_types},
-}} from '../types/{types_filename}';
-import * as types from '../types/{types_filename}';
+actions_file_template = '''import * as types from '../types/{types_filename}';
 
 
 {actions}'''
@@ -42,32 +24,10 @@ reducer_case_template = '''
   }}
 '''
 
-reducers_file_template = '''// @flow
-import {{ combineReducers }} from 'redux';
+reducers_file_template = '''import {{ combineReducers }} from 'redux';
 
-import type {{ ID_TYPE, ERROR_TYPE }} from '../types/common';
-import type {{ {managed_type} }} from '../types/{types_filename}';
-import type {{ SubstateMultiplexerStateType }} from './common/substateMultiplexer';
 import * as common from './common';
 import * as types from '../types/{types_filename}';
-
-export type {domain_state_type} = {{
-  byId: {{ [ID_TYPE]: {managed_type} }},
-  order: Array<ID_TYPE>,
-  fetching: Array<ID_TYPE>,
-  isFetching: boolean,
-  error: ERROR_TYPE,
-  errors: {{ [ID_TYPE]: ERROR_TYPE }},
-  isToggled: boolean,
-  selected: number,
-  counter: number,
-  substateMultiplexer: SubstateMultiplexerStateType,
-  orderById: {{ [ID_TYPE]: Array<ID_TYPE> }},
-  timestamp: number,
-  nextPage: ?number,
-  keyExtractorById: {{ [ID_TYPE]: mixed }},
-  singleton: Object,
-}};
 
 const byId = common.byId({{
   added: [],
@@ -198,33 +158,28 @@ export default {reducer_name};
 
 
 // Selectors
-export const get{singular_managed_type_upper_camel_cased} = (state: {domain_state_type}, id: ID_TYPE): ?{managed_type} => state.byId[id];
-export const get{plural_managed_type_upper_camel_cased} = (state: {domain_state_type}): Array<?{managed_type}> => state.order.map(i => get{singular_managed_type_upper_camel_cased}(state, i));
-export const is{singular_managed_type_upper_camel_cased}Fetching = (state: {domain_state_type}, id: ID_TYPE): boolean => state.fetching.includes(id);
-export const isFetching{plural_managed_type_upper_camel_cased} = (state: {domain_state_type}): boolean => state.isFetching;
-export const get{plural_managed_type_upper_camel_cased}Error = (state: {domain_state_type}): ERROR_TYPE => state.error;
-export const get{singular_managed_type_upper_camel_cased}Error = (state: {domain_state_type}, id: ID_TYPE): ERROR_TYPE => state.errors[id];
-export const are{plural_managed_type_upper_camel_cased}Toggled = (state: {domain_state_type}): boolean => state.toggle;
-export const getSelected{singular_managed_type_upper_camel_cased} = (state: {domain_state_type}): ?{managed_type} => get{singular_managed_type_upper_camel_cased}(state, state.selected);
-export const get{singular_managed_type_upper_camel_cased}Counter = (state: {domain_state_type}): number => state.counter;
-export const getOrderFor = (state: {domain_state_type}, id: ID_TYPE): Array<ID_TYPE>? => state.orderById[id];
-export const getTimestamp = (state: {domain_state_type}): number => state.timestamp;
-export const getNextPage = (state: {domain_state_type}): ?number => state.nextPage;
-export const getKeyExtractionFor = (state: {domain_state_type}, id: ID_TYPE): mixed => state.keyExtractorById[id];
-export const getSingleton = (state: {domain_state_type}): Object => state.singleton;
+export const get{singular_managed_type_upper_camel_cased} = (state, id) => state.byId[id];
+export const get{plural_managed_type_upper_camel_cased} = state => state.order.map(i => get{singular_managed_type_upper_camel_cased}(state, i));
+export const is{singular_managed_type_upper_camel_cased}Fetching = (state, id) => state.fetching.includes(id);
+export const isFetching{plural_managed_type_upper_camel_cased} = state => state.isFetching;
+export const get{plural_managed_type_upper_camel_cased}Error = state => state.error;
+export const get{singular_managed_type_upper_camel_cased}Error = (state, id) => state.errors[id];
+export const are{plural_managed_type_upper_camel_cased}Toggled = state => state.toggle;
+export const getSelected{singular_managed_type_upper_camel_cased} = state => get{singular_managed_type_upper_camel_cased}(state, state.selected);
+export const get{singular_managed_type_upper_camel_cased}Counter = state => state.counter;
+export const getOrderFor = (state, id) => state.orderById[id];
+export const getTimestamp = state => state.timestamp;
+export const getNextPage = state => state.nextPage;
+export const getKeyExtractionFor = (state, id) => state.keyExtractorById[id];
+export const getSingleton = state => state.singleton;
 
 //////////////////////////////////////////////////////////////
 // TODO: move to index.js
 //////////////////////////////////////////////////////////////
 
 // Imports
-import type {{ {domain_state_type} }} from './{reducer_filename}';
 import {reducer_name}, * as from{plural_managed_type_upper_camel_cased} from './{reducer_filename}';
 
-// AppState
-export type AppState = {{
-  {reducer_name}: {domain_state_type}
-}};
 
 // Reducer
 const reducer = combineReducers({{
@@ -245,8 +200,9 @@ export const getSingleton = genSelector(from{plural_managed_type_upper_camel_cas
 
 /*
 const {reducer_name} =  (
-  state: {domain_state_type} = {{}},
-  action: {main_action_type_name}): {domain_state_type} => {{
+  state = {{}},
+  action,
+) => {{
   switch (action.type) {{
     {cases}
     default:
